c21524d462561de6c98b58f4b3607167bf42b2de
janocee <janet.cheung86@gmail.com>
2014-04-07 17:11:32 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,7 +24,7 @@ describe "UserPages" do
       describe "after submission" do 
         before { click_button submit }
 
-        it { shoudl have_title('Sign up') }
+        it { should have_title('Sign up') }
         it { should have_content('error') }
       end
     end
@@ -37,16 +37,13 @@ describe "UserPages" do
         fill_in "Confirmation", with: "foobar"
       end
 
-      describe "after saving the user" do 
+      describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
-
-      it "should create a user" do 
-        expect { click_button }.to change(User, :count).by(1)
       end
     end
   end
