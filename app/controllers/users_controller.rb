e354f2fd1e23981044fe97332b6acda4ce851d6f
janocee <janet.cheung86@gmail.com>
2014-04-10 11:59:42 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
 	def show
 		@user = User.find(params[:id])
 	end
 
   def new
-  	@user = User.new
+    unless signed_in?
+      @user = User.new
+    else
+      flash[:info] = "You're already logged in, so you cannot create a new account."
+      redirect_to root_path
+    end
   end
 
   def create
@@ -19,8 +31,47 @@ class UsersController < ApplicationController
   	end 
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    unless current_user?(user)
+      user.destroy
+      flash[:success] = "User deleted."
+    end
+    redirect_to users_url
+  end
+
   private
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in." 
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?    
   end
 end
