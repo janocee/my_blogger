2372a398ba02d4655ba0a43c87c1095941066643
janocee <janet.cheung86@gmail.com>
2014-04-14 13:32:24 -0400

    Add user microposts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on the per-page basis.
 	def full_title(page_title)
-		base_title = "Ruby on Rails Tutorial Blogger"
+		base_title = "Ruby on Rails Tutorial Jano's Practice App"
 		if page_title.empty?
 			base_title
 		else
